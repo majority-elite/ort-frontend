7c27f2226bbc5ca4a90a71d01a1f1db28edc5607
aube-dev <rufflain918@gmail.com>
2024-07-24 02:32:12 +0900

    fix: remove unnecessary `remix.config.js`

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildPath: 'functions/[[path]].js',
-  serverConditions: ['worker'],
-  serverDependenciesToBundle: 'all',
-  serverMainFields: ['browser', 'module', 'main'],
-  serverMinify: true,
-  serverPlatform: 'neutral',
-};
