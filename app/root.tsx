1ff8b980f4ae8a82147f15ed133b3a6ac936c1f9
aube-dev <rufflain918@gmail.com>
2024-07-23 18:08:51 +0900

    feat: init project

---
+++ app/root.tsx
@@ -0,0 +1,27 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+export const Layout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="ko">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+    </body>
+  </html>
+);
+
+const App = () => <Outlet />;
+
+export default App;
---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import type { MetaFunction } from '@remix-run/node';
+
+export const meta: MetaFunction = () => [
+  { title: 'ORT - 동아리 플랫폼' },
+  {
+    name: 'description',
+    content:
+      '동아리를 찾고, 동아리에 들어가, 동아리에서 활동하는 모든 과정에 함께합니다.',
+  },
+];
+
+const Index = () => (
+  <div>
+    <h1>Ort</h1>
+  </div>
+);
+
+export default Index;
