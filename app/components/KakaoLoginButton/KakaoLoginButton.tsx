e91bc63b234ed0e941eaccbc9245f0ea82cd35a8
crew852 <65230973+crew852@users.noreply.github.com>
2024-08-16 17:38:59 +0900

    feat: edit uuid parse for cloudflare

--- app/components/KakaoLoginButton/KakaoLoginButton.tsx
+++ app/components/KakaoLoginButton/KakaoLoginButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const clientId = 'f5aa2f20e42d783654b8e8c01bfc6312';
@@ -7,14 +7,18 @@ const redirectUri = 'http://localhost:5173/oauth/kakao';
 
 //이용자의 uuid를 받아와 state값으로 사용합니다.
 const getUserUUID = (): string => uuidv4();
-const userUUID = getUserUUID();
 
-const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&state=${userUUID}`;
+const KakaoLoginButton: React.FC = () => {
+  const kakaoAuthUrl = useMemo(() => {
+    const userUUID = getUserUUID();
+    return `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&state=${userUUID}`;
+  }, []);
 
-const KakaoLoginButton: React.FC = () => (
-  <a href={kakaoAuthUrl}>
-    <button>카카오로 로그인</button>
-  </a>
-);
+  return (
+    <a href={kakaoAuthUrl}>
+      <button>카카오로 로그인</button>
+    </a>
+  );
+};
 
 export default KakaoLoginButton;
