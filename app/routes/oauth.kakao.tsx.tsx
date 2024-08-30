505907ddd4a70bdab97975cf89ca8aeba016e4ee
crew852 <65230973+crew852@users.noreply.github.com>
2024-08-30 17:26:40 +0900

    feat: fix package, util name

--- app/routes/oauth.kakao.tsx.tsx
+++ app/routes/oauth.kakao.tsx.tsx
@@ -1,21 +1,15 @@
 import { LoaderFunction, json } from '@remix-run/cloudflare';
-import axios, { AxiosError } from 'axios';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const code = url.searchParams.get('code');
   const state = url.searchParams.get('state');
 
+  // 이 아래의 try문 지우고 이어서 하면 됩니다
   try {
-    const response = await axios.post('(백엔드api url)', {
-      code,
-      state,
-    });
-    return json({ message: 'Success', data: response.data });
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    console.error('Error sending code to backend:', axiosError);
-    return json({ message: 'Error', error: axiosError.message });
+    console.log(code, state);
+  } catch {
+    console.log(json);
   }
 };
 
