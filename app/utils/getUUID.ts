e0c28425025b64c096353da2bf6d533d136e5057
crew852 <65230973+crew852@users.noreply.github.com>
2024-08-30 16:56:38 +0900

    Update app/utils/getUUID.ts
    Co-authored-by: Gwangseo Go <rufflain918@gmail.com>

--- app/utils/getUUID.ts
+++ app/utils/getUUID.ts
@@ -1,9 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
 //이용자의 uuid를 받아옵니다.
-const getUUID = () => {
-  const getUserUUID = (): string => uuidv4();
-  return getUserUUID;
-};
+const getUUID = () => uuidv4();
 
 export default getUUID;
