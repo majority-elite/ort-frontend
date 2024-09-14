72395a6a4e58d2df4d8f16a13db8279e3829e127
aube-dev <rufflain918@gmail.com>
2024-09-14 19:20:56 +0900

    fix: load dev proxy plugin only on dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,25 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { remixCloudflareDevProxyVitePlugin } from './server';
 
-export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxyVitePlugin(),
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-    vanillaExtractPlugin(),
-  ],
+export default defineConfig(async ({ command }) => {
+  const { remixCloudflareDevProxyVitePlugin } =
+    command === 'serve'
+      ? await import('./server')
+      : { remixCloudflareDevProxyVitePlugin: undefined };
+
+  return {
+    plugins: [
+      remixCloudflareDevProxyVitePlugin?.(),
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      tsconfigPaths(),
+      vanillaExtractPlugin(),
+    ].filter((item) => item !== undefined),
+  };
 });
