6068e0af945767dfe12cacd780cc83b6a73ebb1c
aube-dev <rufflain918@gmail.com>
2024-09-16 19:44:21 +0900

    feat: add regex to check `API_URL`

--- server/constants/env.ts
+++ server/constants/env.ts
@@ -1,10 +1,14 @@
 import { z } from 'zod';
 
 export const clientEnvSchema = z.object({
-  API_URL: z.string(),
+  API_URL: z.string().regex(/^(.*[^/])$/g, {
+    message: 'API 주소는 슬래시(/)로 끝나지 않아야 합니다.',
+  }),
 });
 
 export const serverEnvSchema = z.object({
   AUTH_COOKIE_SESSION_SECRET: z.string(),
-  API_URL: z.string(),
+  API_URL: z.string().regex(/^(.*[^/])$/g, {
+    message: 'API 주소는 슬래시(/)로 끝나지 않아야 합니다.',
+  }),
 });
